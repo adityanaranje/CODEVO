d8532ba50551ca74d66de24d501a84a4ec7bc0e2
Aditya Naranje <75174363+adityanaranje@users.noreply.github.com>
2025-09-17 22:33:15 +0530

    Delete utils/file_loader.py

--- utils/file_loader.py
+++
@@ -1,9 +0,0 @@
-def load_uploaded_files(uploaded_files):
-    repo_text = ""
-    for file in uploaded_files:
-        try:
-            content = file.read().decode("utf-8")
-            repo_text += f"\n\n📂 FILE: {file.name}\n{'-'*50}\n{content}\n"
-        except Exception:
-            continue
-    return repo_text
